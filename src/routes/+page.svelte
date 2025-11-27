6a8ad10f6222340798201a006e158ab987b5b9ea
8ball030 <8baller@station.codes>
2025-11-27 16:21:40 +0000

    11th-commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <!-- We add in the background and center everything -->
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
+	let count = 0;
 </script>
 
-<main class="flex min-h-screen flex-col items-center justify-center bg-gray-50 p-4">
+<main class="flex min-h-screen flex-col items-center justify-center bg-primary p-4 text-primary-foreground">
 	<h1>Welcome to SvelteKit</h1>
 	<p>
 		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
 	</p>
 
-	<Button>Click Me</Button>
+	<p class="mt-4">Count: {count}</p>
+
+	<Button onclick={() => count++} variant="secondary">Increment</Button>
 </main>
